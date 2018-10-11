b8220f9015fb139d87170f34b059acea767a3fd3
Nikolay <stoynov96@gmail.com>
2018-10-10 21:49:08 -0700

    Added DataNode interface
    This interface will be used to create objects to write to the database. This should force proper labeling of datapoints to keep our data organized

---
+++ app/src/main/java/com/budget_buddy/utils/DataNode.java
@@ -0,0 +1,11 @@
+package com.budget_buddy.utils;
+
+import java.util.Map;
+
+public interface DataNode {
+    /**
+     * Converts this object to a map that will be written to the database
+     * @return Map to be written to the database
+     */
+    Map<String, Object> ToMap();
+}
--- app/src/main/java/com/budget_buddy/utils/DummyUser.java
+++ app/src/main/java/com/budget_buddy/utils/DummyUser.java
@@ -1,9 +1,12 @@
 package com.budget_buddy.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This class is only here because firebase wants some class to write data. Disregard
  */
-public class DummyUser {
+public class DummyUser implements DataNode {
     public String username;
     public double budget;
 
@@ -14,5 +17,12 @@ public class DummyUser {
     public DummyUser(String username, double budget) {
         this.username = username;
         this.budget = budget;
+    }
+
+    public HashMap<String, Object> ToMap() {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("mapLabelun", username);
+        map.put("mapLabelB", budget);
+        return map;
     }
 }
