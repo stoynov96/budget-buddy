cc088db7027da11f7a1e52cb83d9b5bb7d97a582
Nathan Gelman <nathanjlg@gmail.com>
2018-10-11 18:38:54 -0700

    added experience fraction

--- app/src/main/java/com/budget_buddy/Dashboard.java
+++ app/src/main/java/com/budget_buddy/Dashboard.java
@@ -3,6 +3,7 @@ package com.budget_buddy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import com.budget_buddy.animations.ExperienceBarAnimation;
 
 public class Dashboard extends AppCompatActivity {
@@ -10,6 +11,7 @@ public class Dashboard extends AppCompatActivity {
     BBUser currentUser = BBUser.GetInstance();
 
     ProgressBar experienceBar;
+    TextView experienceProgressText;
     ExperienceBarAnimation experienceBarAnimation;
 
     @Override
@@ -18,12 +20,12 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
 
         experienceBar = findViewById(R.id.experienceBar);
-        experienceBarAnimation = new ExperienceBarAnimation(experienceBar);
-        experienceBarAnimation.setProgress(90);
-        //setExperienceBar(90);
+        experienceBar.setMax(2500);
+        experienceProgressText = findViewById(R.id.experienceProgessFraction);
+        experienceBarAnimation = new ExperienceBarAnimation(experienceBar, experienceProgressText);
+        experienceBarAnimation.setProgress(1675);
+
     }
 
-    private void setExperienceBar(int experience) {
-        //experienceBarAnimation.setProgress(90);
-    }
+
 }
--- app/src/main/java/com/budget_buddy/animations/ExperienceBarAnimation.java
+++ app/src/main/java/com/budget_buddy/animations/ExperienceBarAnimation.java
@@ -3,11 +3,13 @@ package com.budget_buddy.animations;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 // https://stackoverflow.com/a/23156398/10417393
 
 public class ExperienceBarAnimation extends Animation {
     private ProgressBar progressBar;
+    private TextView experienceTextView;
     private long animationDuration = 2000;
     private long stepDuration;
     private int moveTo;
@@ -16,6 +18,13 @@ public class ExperienceBarAnimation extends Animation {
     public ExperienceBarAnimation(ProgressBar progressBar) {
         super();
         this.progressBar = progressBar;
+        stepDuration = animationDuration / progressBar.getMax();
+    }
+
+    public ExperienceBarAnimation(ProgressBar progressBar, TextView textView) {
+        super();
+        this.progressBar = progressBar;
+        this.experienceTextView = textView;
         stepDuration = animationDuration / progressBar.getMax();
     }
 
@@ -30,7 +39,7 @@ public class ExperienceBarAnimation extends Animation {
         moveFrom = progressBar.getProgress();
 
         //animationDuration = (Math.abs(moveTo - moveFrom) * stepDuration);
-        setDuration(Math.abs(moveTo - moveFrom) * stepDuration);
+        setDuration(animationDuration);
         progressBar.startAnimation(this);
     }
 
@@ -38,6 +47,8 @@ public class ExperienceBarAnimation extends Animation {
     protected void applyTransformation(float interpolatedTime, Transformation t) {
         super.applyTransformation(interpolatedTime, t);
         float value = moveFrom + (moveTo - moveFrom) * interpolatedTime;
-        progressBar.setProgress((int) value);
+        int progress = (int) value;
+        experienceTextView.setText("(" + progress + "/" + progressBar.getMax() + ")");
+        progressBar.setProgress(progress);
     }
 }
