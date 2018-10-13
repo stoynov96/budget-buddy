20a68f7d9c41c6d1c714bec3359dd514f359b5c9
Nathan Gelman <nathanjlg@gmail.com>
2018-10-12 20:05:49 -0700

    updated dashboard class

--- app/src/main/java/com/budget_buddy/Dashboard.java
+++ app/src/main/java/com/budget_buddy/Dashboard.java
@@ -1,22 +1,46 @@
 package com.budget_buddy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.budget_buddy.animations.ExperienceBarAnimation;
 
 public class Dashboard extends AppCompatActivity {
 
     BBUser currentUser = BBUser.GetInstance();
+
+    ProgressBar experienceBar;
+    TextView experienceProgressText;
+    ExperienceBarAnimation experienceBarAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-        TextView userNameTextView = findViewById(R.id.userName);
-        userNameTextView.setText("User: " + currentUser.GetUserName());
+
+        setupExperienceBar();
     }
+
+    private void setupExperienceBar() {
+        experienceBar = findViewById(R.id.experienceBar);
+        experienceBar.setMax(2500);
+        experienceProgressText = findViewById(R.id.experienceProgessFraction);
+        experienceBarAnimation = new ExperienceBarAnimation(experienceBar, experienceProgressText);
+        experienceBarAnimation.setProgress(1675);
+    }
+
+    public void gotoEntryMethodFor(View view) {
+        if(view.getId() == R.id.manualEntry) {
+
+        } else if (view.getId() == R.id.cameraEntry) {
+
+        } else {
+            return;
+        }
+    }
+
+
 }
