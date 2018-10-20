54b8b333037d5060be093134eb4d39f117de4087
Nathan Gelman <nathanjlg@gmail.com>
2018-10-19 20:22:16 -0700

    added logic to set date when user picks one and also set the defualt date to today

--- app/src/main/java/com/budget_buddy/ManualEntry.java
+++ app/src/main/java/com/budget_buddy/ManualEntry.java
@@ -4,19 +4,39 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.budget_buddy.components.DatePickerFragment;
 import com.budget_buddy.exception.InvalidDataLabelException;
 
-public class ManualEntry extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class ManualEntry extends AppCompatActivity implements DatePickerFragment.OnDateSetListener {
 
     BBUser user = BBUser.GetInstance();
+
+    private EditText purchaseDateField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_entry);
+
+        purchaseDateField = findViewById(R.id.purchaseDate);
+        final Calendar calendar = Calendar.getInstance();
+        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        purchaseDateField.setText(dateFormat.format(calendar.getTime()));
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        final Calendar calendar = Calendar.getInstance();
+        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+
+        calendar.set(year, month, day);
+        purchaseDateField.setText(dateFormat.format(calendar.getTime()));
     }
 
     /**
--- app/src/main/java/com/budget_buddy/components/DatePickerFragment.java
+++ app/src/main/java/com/budget_buddy/components/DatePickerFragment.java
@@ -1,15 +1,35 @@
 package com.budget_buddy.components;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.app.DatePickerDialog;
+import android.util.Log;
 import android.widget.DatePicker;
 
 import java.util.Calendar;
 
 // https://developer.android.com/guide/topics/ui/controls/pickers
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    public interface OnDateSetListener {
+        void onDateSet(DatePicker view, int year, int month, int day);
+    }
+
+    private OnDateSetListener onDateSetListener;
+
+    @Override
+    public void onAttach(Context activity) {
+        super.onAttach(activity);
+        // https://stackoverflow.com/a/42889134/10417393
+
+        try {
+            onDateSetListener = (OnDateSetListener) activity;
+        } catch (ClassCastException e) {
+            Log.d("Date Listener", "activity does not implement OnDateSetListener");
+        }
+    }
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -24,6 +44,8 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
     }
 
     public void onDateSet(DatePicker view, int year, int month, int day) {
-
+        if (onDateSetListener != null) {
+            onDateSetListener.onDateSet(view, year, month, day);
+        }
     }
 }
