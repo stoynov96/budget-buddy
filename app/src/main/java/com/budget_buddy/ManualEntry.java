1cc58c0c78b07aca26a18ce95430abe377f2ee51
Nathan Gelman <nathanjlg@gmail.com>
2018-10-21 12:14:20 -0700

    Date picker (#47)
    * added date picker fragment and launch on click of text field
    
    * added logic to set date when user picks one and also set the defualt date to today
    
    * added some comments

--- app/src/main/java/com/budget_buddy/ManualEntry.java
+++ app/src/main/java/com/budget_buddy/ManualEntry.java
@@ -1,21 +1,43 @@
 package com.budget_buddy;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
+import com.budget_buddy.components.DatePickerFragment;
 import com.budget_buddy.exception.InvalidDataLabelException;
 
-public class ManualEntry extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class ManualEntry extends AppCompatActivity implements DatePickerFragment.OnDateSetListener {
 
     BBUser user = BBUser.GetInstance();
+
+    private EditText purchaseDateField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_entry);
+
+        purchaseDateField = findViewById(R.id.purchaseDate);
+        final Calendar calendar = Calendar.getInstance();
+        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        purchaseDateField.setText(dateFormat.format(calendar.getTime()));
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        final Calendar calendar = Calendar.getInstance();
+        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+
+        calendar.set(year, month, day);
+        purchaseDateField.setText(dateFormat.format(calendar.getTime()));
     }
 
     /**
@@ -44,5 +66,15 @@ public class ManualEntry extends AppCompatActivity {
 
         // end purchase entry
         finish();
+    }
+
+    /**
+     * This function is called when tapping the date field. Instead of focusing on the field,
+     * a date picker dialog pops up.
+     * @param view
+     */
+    public void displayDatepicker(View view) {
+        DialogFragment datePickerFragment = new DatePickerFragment();
+        datePickerFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
