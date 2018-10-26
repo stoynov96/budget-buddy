e6e19cc053c26b979bb382f5f64b30b5ee16af81
Nathan Gelman <nathanjlg@gmail.com>
2018-10-26 02:52:34 -0700

    basic computer vision

--- app/src/main/java/com/budget_buddy/PhotoEntry.java
+++ app/src/main/java/com/budget_buddy/PhotoEntry.java
@@ -1,25 +1,47 @@
 package com.budget_buddy;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.hardware.camera2.CameraCaptureSession;
 import android.util.Log;
 import android.util.Size;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+
+
+import android.util.SparseIntArray;
 import android.view.TextureView;
 import android.view.Surface;
+import android.view.View;
 
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 // http://coderzpassion.com/android-working-camera2-api/
 
@@ -31,13 +53,109 @@ public class PhotoEntry extends AppCompatActivity {
     private CaptureRequest.Builder previewBuilder;
     private  CameraCaptureSession previewSession;
 
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo_entry);
 
+        setContentView(R.layout.activity_photo_entry);
         cameraTextureView =(TextureView) findViewById(R.id.textureView);
         cameraTextureView.setSurfaceTextureListener(surfaceTextureListener);
+    }
+
+    public void getPicture(View view) {
+        if(cameraDevice == null) {
+            return;
+        }
+        CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            CameraCharacteristics characteristics = manager.getCameraCharacteristics(cameraDevice.getId());
+
+            if(characteristics != null) {
+
+            }
+
+            ImageReader reader = ImageReader.newInstance(640,480,ImageFormat.YUV_420_888,1);
+            List<Surface> outputSurfaces = new ArrayList<Surface>(2);
+            outputSurfaces.add(reader.getSurface());
+            outputSurfaces.add(new Surface(cameraTextureView.getSurfaceTexture()));
+
+            final CaptureRequest.Builder captureBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            captureBuilder.addTarget(reader.getSurface());
+            captureBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+
+            final int rotation = getRotationCompensation(cameraDevice.getId(), this, this);
+
+            ImageReader.OnImageAvailableListener imageAvailableListener = new ImageReader.OnImageAvailableListener() {
+                @Override
+                public void onImageAvailable(ImageReader reader) {
+                    Image image = null;
+                    try {
+                        image = reader.acquireNextImage();
+                        //ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                        //byte[] bytes = new byte[buffer.capacity()];
+                        FirebaseVisionImage fbImage = FirebaseVisionImage.fromMediaImage(image, rotation);
+                        analyzeImage(fbImage);
+
+                    } catch (Exception e) {
+                        Log.i("Image", e.toString());
+                    }
+                }
+
+            };
+
+            HandlerThread handlerThread=new HandlerThread("takepicture");
+            handlerThread.start();
+
+            final Handler handler=new Handler(handlerThread.getLooper());
+            reader.setOnImageAvailableListener(imageAvailableListener,handler);
+
+
+            final CameraCaptureSession.CaptureCallback  previewSSession=new CameraCaptureSession.CaptureCallback() {
+                @Override
+                public void onCaptureStarted(CameraCaptureSession session, CaptureRequest request, long timestamp, long frameNumber) {
+                    super.onCaptureStarted(session, request, timestamp, frameNumber);
+                }
+
+                @Override
+                public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
+                    super.onCaptureCompleted(session, request, result);
+                    startCamera();
+                }
+            };
+
+            cameraDevice.createCaptureSession(outputSurfaces, new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+
+                    try
+                    {
+                        session.capture(captureBuilder.build(),previewSSession,handler);
+
+                    }catch (Exception e)
+                    {
+
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+
+                }
+            },handler);
+
+        } catch (Exception e) {
+            Log.i("Image", e.toString());
+        }
     }
 
     public void openCamera() {
@@ -155,6 +273,70 @@ public class PhotoEntry extends AppCompatActivity {
         } catch (Exception e) {
 
         }
+    }
+
+    private void analyzeImage(FirebaseVisionImage image) {
+        FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
+        textRecognizer.processImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+            @Override
+            public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                String text = firebaseVisionText.getText();
+                Log.i("Image text", text);
+                /*List<FirebaseVisionText.TextBlock> blocks = firebaseVisionText.getTextBlocks();
+
+                for(int i = 0; i < blocks.size(); i++) {
+                    List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+                    for(int j = 0; j < lines.size(); j++) {
+                        List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                        for (int k = 0; k < elements.size())
+                    }
+                }*/
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.i("Image text", "failure");
+            }
+        });
+    }
+
+    private int getRotationCompensation(String cameraId, Activity activity, Context context)
+            throws CameraAccessException {
+        // Get the device's current rotation relative to its "native" orientation.
+        // Then, from the ORIENTATIONS table, look up the angle the image must be
+        // rotated to compensate for the device's rotation.
+        int deviceRotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int rotationCompensation = ORIENTATIONS.get(deviceRotation);
+
+        // On most devices, the sensor orientation is 90 degrees, but for some
+        // devices it is 270 degrees. For devices with a sensor orientation of
+        // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
+        CameraManager cameraManager = (CameraManager) context.getSystemService(CAMERA_SERVICE);
+        int sensorOrientation = cameraManager
+                .getCameraCharacteristics(cameraId)
+                .get(CameraCharacteristics.SENSOR_ORIENTATION);
+        rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360;
+
+        // Return the corresponding FirebaseVisionImageMetadata rotation value.
+        int result;
+        switch (rotationCompensation) {
+            case 0:
+                result = FirebaseVisionImageMetadata.ROTATION_0;
+                break;
+            case 90:
+                result = FirebaseVisionImageMetadata.ROTATION_90;
+                break;
+            case 180:
+                result = FirebaseVisionImageMetadata.ROTATION_180;
+                break;
+            case 270:
+                result = FirebaseVisionImageMetadata.ROTATION_270;
+                break;
+            default:
+                result = FirebaseVisionImageMetadata.ROTATION_0;
+                //Log.e(TAG, "Bad rotation value: " + rotationCompensation);
+        }
+        return result;
     }
 
 }
