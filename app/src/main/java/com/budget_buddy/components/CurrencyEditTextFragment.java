44a4df1ca54392218aedc78b8c942984833bbc12
Kevin Bulosan <bulosan@u-10-36-5-158.dhcp.unlv.edu>
2018-10-29 17:28:27 -0700

    Update app/src/main/java/com/budget_buddy/components/CurrencyEditTextFragment.java

--- app/src/main/java/com/budget_buddy/components/CurrencyEditTextFragment.java
+++ app/src/main/java/com/budget_buddy/components/CurrencyEditTextFragment.java
@@ -4,15 +4,18 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.support.v7.widget.AppCompatEditText;
 import android.text.Editable;
+import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Log;
 
+import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public class CurrencyEditTextFragment extends AppCompatEditText{
 
+    private final int MAX_LENGTH = 20;
     private String previousNumber;
 
     TextWatcher textWatcher = new TextWatcher() {
@@ -30,36 +33,48 @@ public class CurrencyEditTextFragment extends AppCompatEditText{
         public void afterTextChanged(Editable s) {
             // Get string to work with
             String str = s.toString();
-            if (str.equals(".")){
-                return;
-            }
-            if (str.contains(".")){
-                DecimalFormat format = new DecimalFormat("##.00");
-                format.setRoundingMode(RoundingMode.DOWN);
-                Log.i("oh jeez", format.format(str).toString());
-                CurrencyEditTextFragment.super.setText(format.format(str).toString());
-            }
             // Prevent recursive call for afterTextChanged
             if (str.equals(previousNumber) || str.isEmpty()){
                 return;
             }
             previousNumber = str;
+            if (str.equals(".")){
+                return;
+            }
+            if (str.contains(".")){
+                BigDecimal parsed = new BigDecimal(str);
+                DecimalFormat format = new DecimalFormat("#.#");
+                format.setRoundingMode(RoundingMode.DOWN);
+                Log.i("oh jeez", format.format(parsed));
+                // Remove before we set the text
+                CurrencyEditTextFragment.super.removeTextChangedListener(this);
+                CurrencyEditTextFragment.super.setText(format.format(parsed));
+                setSelection();
+                // Add it back
+                CurrencyEditTextFragment.super.addTextChangedListener(this);
+            }
+
+
+
         }
     };
 
     public CurrencyEditTextFragment(Context context) {
         super(context);
         this.addTextChangedListener(textWatcher);
+        //this.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL);
     }
 
     public CurrencyEditTextFragment(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.addTextChangedListener(textWatcher);
+        //this.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL);
     }
 
     public CurrencyEditTextFragment(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         this.addTextChangedListener(textWatcher);
+        //this.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL);
     }
 
     @Override
@@ -74,4 +89,11 @@ public class CurrencyEditTextFragment extends AppCompatEditText{
     }
 
 
+    private void setSelection(){
+        if (CurrencyEditTextFragment.super.getText().length() <= MAX_LENGTH) {
+            CurrencyEditTextFragment.super.setSelection(CurrencyEditTextFragment.super.getText().length());
+        } else {
+            CurrencyEditTextFragment.super.setSelection(MAX_LENGTH);
+        }
+    }
 }
