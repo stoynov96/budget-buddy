e3026887a64d2dcbf167395d9c4ad17e85fc9902
Nathan Gelman <nathanjlg@gmail.com>
2018-11-01 23:36:38 -0700

    added some comments

--- app/src/main/java/com/budget_buddy/PhotoEntry.java
+++ app/src/main/java/com/budget_buddy/PhotoEntry.java
@@ -74,43 +74,6 @@ public class PhotoEntry extends AppCompatActivity {
         cameraTextureView.setSurfaceTextureListener(surfaceTextureListener);
     }
 
-    private void setupImageReader() {
-        try {
-
-            final int rotation = getRotationCompensation(cameraDevice.getId(), this, this);
-            ImageReader.OnImageAvailableListener mImageAvailable = new ImageReader.OnImageAvailableListener() {
-
-                final int frameModulo = 5;
-                int frameCounter = 0;
-                @Override
-                public void onImageAvailable(ImageReader reader) {
-                    Image image = null;
-                    try {
-                        image = reader.acquireNextImage();
-
-                        if(frameCounter % frameModulo == 0) {
-                            FirebaseVisionImage fbImage = FirebaseVisionImage.fromMediaImage(image, rotation);
-                            analyzeImage(fbImage);
-                        }
-                        frameCounter = (frameCounter + 1) % frameModulo;
-                        image.close();
-                    } catch (Exception e) {
-
-                    }
-                }
-            };
-
-            HandlerThread handlerThread=new HandlerThread("analyzepicture");
-            handlerThread.start();
-            final Handler handler = new Handler(handlerThread.getLooper());
-
-            mImageReader.setOnImageAvailableListener(mImageAvailable, handler);
-        } catch (Exception e) {
-
-        }
-
-    }
-
     /**
      * Camera2 boilerplate
      */
@@ -158,7 +121,6 @@ public class PhotoEntry extends AppCompatActivity {
         mImageReader = ImageReader.newInstance(previewSize.getWidth(), previewSize.getHeight(), ImageFormat.YUV_420_888, 1);
         Surface readerSurface = mImageReader.getSurface();
 
-
         try {
             previewBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
         } catch (Exception e) {
@@ -166,10 +128,16 @@ public class PhotoEntry extends AppCompatActivity {
         }
 
         previewBuilder.addTarget(surface);
+        // set the image reader surface as the target of the preview as well so we can analyze the frames
         previewBuilder.addTarget(readerSurface);
 
         try {
             cameraDevice.createCaptureSession(Arrays.asList(surface, readerSurface), new CameraCaptureSession.StateCallback() {
+
+                /**
+                 * This kicks of the threads used to generate the preview and analyze the frames.
+                 * @param session
+                 */
                 @Override
                 public void onConfigured(CameraCaptureSession session) {
                     previewSession = session;
@@ -187,7 +155,10 @@ public class PhotoEntry extends AppCompatActivity {
         }
     }
 
-    void getChangedPreview() {
+    /**
+     * This uses preview builder to continuosly update the cameraTextureView with the next frame.
+     */
+    private void getChangedPreview() {
         if( cameraDevice == null) {
             return;
         }
@@ -204,12 +175,69 @@ public class PhotoEntry extends AppCompatActivity {
         }
     }
 
+    /**
+     * This method initializes the image reader, which is responsible for reading in frame buffers and
+     * analyzing them. In its analysis, every frameModulo'th frame it passes the frame buffer to firebase
+     * for text analysis.
+     */
+    private void setupImageReader() {
+        try {
+
+            final int rotation = getRotationCompensation(cameraDevice.getId(), this, this);
+            ImageReader.OnImageAvailableListener mImageAvailable = new ImageReader.OnImageAvailableListener() {
+
+                final int frameModulo = 5;
+                int frameCounter = 0;
+                @Override
+                public void onImageAvailable(ImageReader reader) {
+                    Image image = null;
+                    try {
+                        image = reader.acquireNextImage();
+
+                        if(frameCounter % frameModulo == 0) {
+                            FirebaseVisionImage fbImage = FirebaseVisionImage.fromMediaImage(image, rotation);
+                            analyzeImage(fbImage);
+                        }
+                        frameCounter = (frameCounter + 1) % frameModulo;
+                        image.close();
+                    } catch (Exception e) {
+
+                    }
+                }
+            };
+
+            HandlerThread handlerThread=new HandlerThread("analyzepicture");
+            handlerThread.start();
+            final Handler handler = new Handler(handlerThread.getLooper());
+
+            mImageReader.setOnImageAvailableListener(mImageAvailable, handler);
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    /**
+     * This function takes in an image and begins analysis on it. See onSuccess() in the OnSuccessListener
+     * @param image
+     */
     private void analyzeImage(final FirebaseVisionImage image) {
         FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
         textRecognizer.processImage(image).addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
 
+            // this needs to be globally accessible across the threads so they can see
+            // when one was successful
             Double price;
 
+            /**
+             * This function reads through all of the blocks of text in result of the image analysis.
+             * Blocks are composed of lines, which are composed of elements which are individual words.
+             * Once total is found, it looks to see if it can find a box that's adjacent to it, which
+             * contains the price.
+             * This function is synchronized to prevent race conditions. This is less efficient than
+             * the other way but this function is hella fast compared to actually doing computer vision.
+             * @param firebaseVisionText
+             */
             @Override
             public void onSuccess(FirebaseVisionText firebaseVisionText) {
                 String text = firebaseVisionText.getText();
@@ -233,6 +261,7 @@ public class PhotoEntry extends AppCompatActivity {
                             List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
 
                             for (int k = 0; k < elements.size(); k++) {
+                                // draw box around the word
                                 GraphicOverlay.Graphic textGraphic = new TextGraphic(mGraphicOverlay, elements.get(k));
                                 mGraphicOverlay.add(textGraphic);
 
@@ -251,10 +280,10 @@ public class PhotoEntry extends AppCompatActivity {
                                     if (onSameLine) {
 
                                         try {
-                                            Log.i("Price", elements.get(k).getText());
+                                            // when the price is found, stop the camera
                                             price = Double.parseDouble(elements.get(k).getText());
-                                            //cameraDevice.close();
                                             previewSession.stopRepeating();
+                                            mImageReader.close();
                                             return;
                                         } catch (Exception e) {
 
@@ -276,6 +305,14 @@ public class PhotoEntry extends AppCompatActivity {
         });
     }
 
+    /**
+     * This is the method of accounting for rotation when passing images into firebase.
+     * @param cameraId
+     * @param activity
+     * @param context
+     * @return
+     * @throws CameraAccessException
+     */
     private int getRotationCompensation(String cameraId, Activity activity, Context context)
             throws CameraAccessException {
         // Get the device's current rotation relative to its "native" orientation.
