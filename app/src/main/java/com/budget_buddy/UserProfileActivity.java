e95c1456ebf187e1e36fcd40cd821cb870f061ea
Nathan Gelman <nathanjlg@gmail.com>
2018-11-12 00:38:33 -0800

    added callbacks and new table reader method to handle multiple callbacks

--- app/src/main/java/com/budget_buddy/UserProfileActivity.java
+++ app/src/main/java/com/budget_buddy/UserProfileActivity.java
@@ -3,13 +3,32 @@ package com.budget_buddy;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+
+import com.budget_buddy.utils.Data.MyCallback;
+
+import java.util.HashMap;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    BBUser currentUser = BBUser.GetInstance();
+
+    EditText monthlyIncomeField;
+    EditText rentField;
+    EditText otherMonthlyExpensesField;
+    EditText monthlySavingsGoalField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        monthlyIncomeField = findViewById(R.id.total_monthy_income_field);
+        rentField = findViewById(R.id.total_rent_field);
+        otherMonthlyExpensesField = findViewById(R.id.total_other_expenses_field);
+        monthlySavingsGoalField = findViewById(R.id.monthly_savings_goal_field);
+
+        currentUser.addUICallback(profileCallback);
     }
 
     public void saveEntry(View view) {
@@ -19,4 +38,25 @@ public class UserProfileActivity extends AppCompatActivity {
     public void cancelEntry(View view) {
         finish();
     }
+
+    MyCallback profileCallback = new MyCallback() {
+        @Override
+        public void OnCallback(float[] weeklySpending) { }
+
+        @Override
+        public void OnCallback(HashMap<String, Object> map) { }
+
+        @Override
+        public void OnProfileSet() {
+            long monthlyIncome, rent, otherExpenses, savingsGoal;
+            monthlyIncome = currentUser.getPrimaryIncome();
+            rent = currentUser.getRent();
+            otherExpenses = currentUser.getOtherExpenses();
+            savingsGoal = currentUser.getSavingsGoal();
+            monthlyIncomeField.setText("$" + (monthlyIncome == -1 ? "0.00" : monthlyIncome));
+            rentField.setText("$" + (rent == -1 ? "0.00" : rent));
+            otherMonthlyExpensesField.setText("$" + (otherExpenses == -1 ? "0.00" : otherExpenses));
+            monthlySavingsGoalField.setText("$" + (savingsGoal == -1 ? "0.00" : savingsGoal));
+        }
+    };
 }
