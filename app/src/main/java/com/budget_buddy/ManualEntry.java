4dbe9dd4f1247c0daf49bae452ceffba3565c7ae
jackmackey <43592433+jackmackey@users.noreply.github.com>
2018-11-11 22:18:49 -0800

    - Fixed the null-user crash issue. (#77) and added title guessing/ manual title options
    - Added some better checking for the total cost of an expenditure (case-insensitive, more words to look for).
    - Added a regular expression check when searching for a price so the camera only stops scanning once it finds something that is a valid price.
    - Now auto-populate the expenditure name field with the top-most item read from the receipt (this is generally not the name we want).
    - Added a drop-down menu containing all the parsed lines from the receipt to the ManualEntry display which can be used for title correction.
    - Boxes are now drawn around lines on the receipt rather than each word. Makes it less cluttered and coincides with how the drop-down menu works.
    - Added an id to the ManualEntry layout so it can be referenced programmatically.
    - Added an initializer to the TextGraphic class which takes lines as a parameter instead of elements. Needed to draw boxes around lines instead of words.
    - Modified the draw function in the TextGraphic class to draw depending on how it was initialized (either using lines or words).

--- app/src/main/java/com/budget_buddy/ManualEntry.java
+++ app/src/main/java/com/budget_buddy/ManualEntry.java
@@ -1,22 +1,33 @@
 package com.budget_buddy;
 
 import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 
 import com.budget_buddy.components.CurrencyEditTextFragment;
 import com.budget_buddy.exception.InvalidDataLabelException;
 import android.util.Log;
 import android.widget.DatePicker;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.budget_buddy.components.DatePickerFragment;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.logging.Level;
 
 public class ManualEntry extends AppCompatActivity implements DatePickerFragment.OnDateSetListener {
 
@@ -45,6 +56,61 @@ public class ManualEntry extends AppCompatActivity implements DatePickerFragment
             amountField.setText(price);
             amountField.validate();
         }
+
+        // This contains the parsed lines from the receipt. Used to make a drop-down selector for
+        // the item name. Could possibly do some fancy stuff here to make the options better, like
+        // various regular expressions to make better options (like removing the restaurant number
+        // from a chain name, for example).
+        // This can (probably should) be modified to split the array into two arrays: one of text, one of numbers matching a price format.
+        // Then we can allow for both title and price correction
+        ArrayList<String> options = getIntent().getStringArrayListExtra("options");
+        if(options != null) {
+            ConstraintLayout layout = findViewById(R.id.manualEntryLayout);
+            ConstraintSet set = new ConstraintSet();
+            set.clone(layout);
+
+            final EditText editText = (EditText)findViewById(R.id.purchaseName);
+            final Spinner spinner = new Spinner(this);
+            ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, options);
+
+            spinner.setAdapter(spinnerAdapter);
+            spinner.setId(View.generateViewId());
+            spinner.setTag("nameSpinner");
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    editText.setText(spinner.getSelectedItem().toString());
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+
+            editText.setText(options.get(0));
+            layout.addView(spinner);
+
+            TextView optionsText = new TextView(this);
+            optionsText.setText("Purchase Title Options:");
+            optionsText.setId(View.generateViewId());
+            optionsText.setTag("optionsHeader");
+            layout.addView(optionsText);
+
+            set.constrainHeight(optionsText.getId(), 50);
+            set.constrainHeight(spinner.getId(), 50);
+            set.connect(optionsText.getId(), ConstraintSet.TOP, R.id.purchaseAmount, ConstraintSet.BOTTOM, 12);
+            set.connect(optionsText.getId(), ConstraintSet.RIGHT, spinner.getId(), ConstraintSet.LEFT, 0);
+            set.connect(optionsText.getId(), ConstraintSet.BOTTOM, spinner.getId(), ConstraintSet.BOTTOM, 0);
+            set.connect(optionsText.getId(), ConstraintSet.START, R.id.purchaseAmountTitle, ConstraintSet.START, 0);
+            set.connect(spinner.getId(), ConstraintSet.TOP, R.id.purchaseAmount, ConstraintSet.BOTTOM, 12);
+            set.connect(spinner.getId(), ConstraintSet.LEFT, optionsText.getId(), ConstraintSet.RIGHT, 0);
+            set.connect(spinner.getId(), ConstraintSet.BOTTOM, R.id.purchaseDescriptionTitle, ConstraintSet.TOP, 0);
+            set.connect(spinner.getId(), ConstraintSet.RIGHT, R.id.purchaseAmount, ConstraintSet.RIGHT, 0);
+            set.connect(R.id.purchaseDescriptionTitle, ConstraintSet.TOP, spinner.getId(), ConstraintSet.BOTTOM, 12);
+
+            set.applyTo(layout);
+       }
     }
 
     @Override
