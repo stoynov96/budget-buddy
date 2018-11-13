9ce5d8a7c4b1221844bb778db04c34ca6d468acd
Nathan Gelman <nathanjlg@gmail.com>
2018-11-12 16:50:34 -0800

    User profile backend work (#79)
    * added a capture button that only allows capture when it's pressed
    
    * added empty actions to buttons
    
    * added callbacks and new table reader method to handle multiple callbacks

--- app/src/main/java/com/budget_buddy/utils/Data/TableReader.java
+++ app/src/main/java/com/budget_buddy/utils/Data/TableReader.java
@@ -1,6 +1,7 @@
 package com.budget_buddy.utils.Data;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.budget_buddy.config.DataConfig;
@@ -88,6 +89,37 @@ public class TableReader {
             lastLabel = label;
         }
         return labelsSb.toString();
+    }
+
+    public void addListener(String path, final MyCallback callback) {
+        Query mQueryReference = mDatabase.child(path);
+
+        mQueryReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                callback.OnProfileSet();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                callback.OnProfileSet();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     /**
