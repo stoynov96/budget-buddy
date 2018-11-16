c8bb2459355db4a5a37dabb24c3ab7bd2726a310
CalamariEater <itskjbulosan@gmail.com>
2018-11-16 12:08:41 -0800

    work
    - pseudo

---
+++ app/src/main/java/com/budget_buddy/Achievements.java
@@ -0,0 +1,18 @@
+package com.budget_buddy;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class Achievements extends AppCompatActivity {
+
+    /*
+    Achievements - Nice way to handle collections besides shoving it in a class?
+     - a collection of private achievements
+        - has a property: public boolean GET ~ if already gotten
+     - a function to check the collection
+        - has a property of when to check it based on what activity we are in
+
+     */
+
+
+
+}
